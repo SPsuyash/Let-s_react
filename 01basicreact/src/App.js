bb046dbb81928891c6d5bde37630e7c7356990c7
SPsuyash <suyashpatelsp@gmail.com>
2024-09-16 16:57:30 +0530

    hey

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>suyash patel bhai</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+
+function App() {
+
+  return (
+    <h1>hello suyash bhai kya haal chal</h1>
+  )
+}
+
+export default App
